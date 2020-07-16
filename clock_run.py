1c5ed4439d2cb95f261a13b401d399d6b5f3be98
lesenpai <levtoniy@gmail.com>
2020-07-16 09:55:49 +0300

    Ingnore src files of clock.exe

--- clock_run.py
+++ clock_run.py
@@ -4,7 +4,7 @@ from clock import *
 clock = Clock()
 while True:
     try:
-        clock.draw(datetime.now())``
+        clock.draw(datetime.now())
         time.sleep(1)
     except KeyboardInterrupt:
         break
